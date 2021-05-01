145d353b6523c8a7cf6bb5c2c8e9273456776f8c
Viliyan Yordanov <42891941+Vili1@users.noreply.github.com>
2021-05-02 00:27:25 +0300

    Pointer maintenance
    Updated for version 2021.4.14s

--- src/fps.cpp
+++ src/fps.cpp
@@ -6,29 +6,56 @@
 
 DWORD dwGetModuleBaseAddress(TCHAR* lpszModuleName, DWORD pID)
 {
-	DWORD dwModuleBaseAddress = 0;
-	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pID);
-	MODULEENTRY32 ModuleEntry32 = { 0 };
-	ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
+    DWORD dwModuleBaseAddress = 0;
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pID);
+    MODULEENTRY32 ModuleEntry32 = { 0 };
+    ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
 
-	if (Module32First(hSnapshot, &ModuleEntry32))
-	{
-		do 
-		{
-			if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0)
-			{
-				dwModuleBaseAddress = (DWORD)ModuleEntry32.modBaseAddr;
-				break;
-			}
-		} while (Module32Next(hSnapshot, &ModuleEntry32));
-	}
-	CloseHandle(hSnapshot);
-	return dwModuleBaseAddress;
+    if (Module32First(hSnapshot, &ModuleEntry32))
+    {
+        do
+        {
+            if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0)
+            {
+                dwModuleBaseAddress = (DWORD)ModuleEntry32.modBaseAddr;
+                break;
+            }
+        } while (Module32Next(hSnapshot, &ModuleEntry32));
+
+    }
+    CloseHandle(hSnapshot);
+    return dwModuleBaseAddress;
 }
 
 int main()
 {
-	HWND hGameWindow;
+    reload:
+    system("CLS");
+    HWND hGameWindow = FindWindow(NULL, "Among Us");
+    if (hGameWindow != NULL)
+    {
+        std::cout << "Among Us found successfully!" << std::endl;
+        std::cout << "------------------------------------------------------" << std::endl;
+    }
+    else
+    {
+        std::cout << "Unable to find Among Us, Please open Among Us!" << std::endl;
+        std::cout << "------------------------------------------------------" << std::endl;
+        Sleep(1000);
+        std::cout << "Auto reloading in 5 seconds!" << std::endl;
+        Sleep(1000);
+        std::cout << "Auto reloading in 4 seconds!" << std::endl;
+        Sleep(1000);
+        std::cout << "Auto reloading in 3 seconds!" << std::endl;
+        Sleep(1000);
+        std::cout << "Auto reloading in 2 seconds!" << std::endl;
+        Sleep(1000);
+        std::cout << "Auto reloading in 1 seconds!" << std::endl;
+        
+        goto reload;
+    }
+
+
 	DWORD pID;
 	HANDLE pHandle;
 	char moduleName[] = "UnityPlayer.dll";
@@ -40,14 +67,15 @@ int main()
 	std::cout << "VSync must be enabled!" << std::endl;
 	while (true)
 	{
-		hGameWindow = FindWindow(NULL, "Among Us");
+	
 		GetWindowThreadProcessId(hGameWindow, &pID);
 		pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
 		DWORD clientBase = dwGetModuleBaseAddress(_T(moduleName), pID);
-		ReadProcessMemory(pHandle, (LPCVOID)(clientBase + 0x0129B6B8), &baseAddress, sizeof(baseAddress), NULL);
-		AdressValue = baseAddress + 0x140;
+		ReadProcessMemory(pHandle, (LPCVOID)(clientBase + 0x0144D3BC), &baseAddress, sizeof(baseAddress), NULL);
+		AdressValue = baseAddress + 0x200;
 		WriteProcessMemory(pHandle, (LPVOID)(AdressValue), &NewAdressValue, sizeof(NewAdressValue), 0);
 		CloseHandle(pHandle);
 		Sleep(1000);
+        goto reload;
 	}
 }
